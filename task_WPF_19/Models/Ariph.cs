ae6a0a66e5a72ed189a6982162c31e3a825b9dca
Валентин Ковальчук <val-81@mail.ru>
2022-01-28 01:27:13 +0300

    Добавьте файлы проекта.

---
+++ task_WPF_19/Models/Ariph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_WPF_19.Models
+{
+    class Ariph
+    {
+        public static int Add(int a, int b) => (int)(2 * Math.PI * a);
+    }
+}
